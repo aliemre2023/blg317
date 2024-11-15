3e4ccdf465935ae4dc751673a06871249691ae3e
aliemre2023 <aliemrekaya239@gmail.com>
2024-11-15 13:39:20 +0300

    api for  player and team count

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from data_fetch import get_countries
+from data_fetch import get_countries, get_numberOfPlayersInCountry, get_numberOfTeamsInCountry
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +18,22 @@ def countries_api():
         'countries': [{'name': country[0], 'flag_link': country[1]} for country in countries],
         'page': page,
         'total_pages': total_pages
+    })
+
+@app.route('/api/numberOfTeams', methods=['GET'])
+def numberOfTeams_api():
+    table = get_numberOfTeamsInCountry()
+
+    return jsonify({
+        'numberOfTeams': [{'country_id': row[0], 'country_name': row[1], 'team_count': row[2]} for row in table],
+    })
+
+@app.route('/api/numberOfPlayers', methods=['GET'])
+def numberOfPlayers_api():
+    table = get_numberOfPlayersInCountry()
+
+    return jsonify({
+        'numberOfPlayers': [{'country_id': row[0], 'country_name': row[1], 'player_count': row[2]} for row in table],
     })
 
 if __name__ == '__main__':
--- src/backend/db_utils.py
+++ src/backend/db_utils.py
@@ -2,6 +2,6 @@ import sqlite3
 
 # General functions to establish a database connection
 def get_db_connection():
-    conn = sqlite3.connect('database.db')  # Update with your database path
+    conn = sqlite3.connect('../db/nba_db.sqlite')  # Update with your database path
     conn.row_factory = sqlite3.Row  # Helps to return rows as dictionaries
     return conn
