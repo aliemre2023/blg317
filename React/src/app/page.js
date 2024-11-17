4d20553ddf35db423791b8b7ce24625b37817ba3
Ferhat Kunduracı <kickmagical@gmail.com>
2024-11-17 21:17:20 +0300

    Made "the Home page"
    To give an idea about the page structure, I prepared a basic main page in which there is not any content now. Just a filler text "Welcome to Home Page" and a navbar which only has 5 countries button. created color variables in globals.css to change the colors easily when needed.

--- React/src/app/page.js
+++ React/src/app/page.js
@@ -7,18 +7,64 @@ function Page() {
     const router = useRouter();
 
     return (
-        <div className="prime-flex flex-row justify-content-center align-content-center h-screen w-screen">
-            <div className="text-center">
-                <div>Welcome to main page</div>
-                <Button
-                    className="mt-2"
+        <div className='h-screen'>
+        <div className="grid mt-1 w-screen bg-primary-reverse">
+            <div className="col-12">
+                <div className="text-center p-3 border-round-sm  font-bold">NBA-DATABASE PROJECT</div>
+            </div>
+        </div>
+        <div className='grid mt-0 w-screen bg-primary-reverse'>
+        <div className='col-2'>
+            <Button
+                    className="mt-0 bg-primary h-full w-full font-bold text-center w-full"
                     onClick={() => {
                         router.push('/countries');
                     }}
-                >
-                    Countries
-                </Button>
+                    label='Countries'
+                > </Button>   
             </div>
+            <div className='col-2'>
+            <Button
+                    className="mt-0 bg-primary h-full w-full font-bold text-center w-full"
+                    onClick={() => {
+                        router.push('/countries');
+                    }}
+                    label='Countries'
+                > </Button>   
+            </div>
+            <div className='col-2'>
+            <Button
+                    className="mt-0 bg-primary h-full w-full font-bold text-center w-full"
+                    onClick={() => {
+                        router.push('/countries');
+                    }}
+                    label='Countries'
+                > </Button>   
+            </div>
+            <div className='col-2'>
+            <Button
+                    className="mt-0 bg-primary h-full w-full font-bold text-center w-full"
+                    onClick={() => {
+                        router.push('/countries');
+                    }}
+                    label='Countries'
+                > </Button>   
+            </div>
+            <div className='col-4'>
+            <Button
+                    className="mt-0 bg-primary h-full w-full font-bold text-center w-full"
+                    onClick={() => {
+                        router.push('/countries');
+                    }}
+                    label='Countries'
+                > </Button>   
+            </div>
+        </div>
+        <div className='prime-flex h-20rem w-screen justify-content-center align-content-center fadeindown animation-ease-in animation-duration-1000'>
+            <div className='text-8xl text-center text-primary'>
+                <div>Welcome To Home Page!</div>
+            </div>
+        </div>
         </div>
     );
 }
