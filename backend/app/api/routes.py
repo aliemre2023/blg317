617a4da87147ff46dbb3fa95a67fd67d0d324781
Ferhat Kunduracı <kickmagical@gmail.com>
2024-12-01 19:06:05 +0300

    Added Info Page for teams
    It's not responsive. I will change that next week

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -127,5 +127,14 @@ def countryTeams_api(country_id):
         'totalCountryTeams': total_countryTeams
     })
 
+@api_bp.route('/teams/<int:teamid>', methods = ['GET'])
+def teamInfo_api(teamid):
+    teamInfo = get_teamInfo(teamid)
+    roster = get_activeRoster(teamid)
+    return jsonify({
+        'teamInfo' : [{'teamName' : teamInfo[1], 'abbreviation' : teamInfo[2] , 'foundedYear' : teamInfo[4], 'owner' : teamInfo[5], 'generalMan' : teamInfo[6], 'headCoach' : teamInfo[7], 'dTeam' : teamInfo[8], 'facebook' : teamInfo[9], 'instagram' : teamInfo[10], 'twitter' : teamInfo[11], 'logo' : teamInfo[12], 'city' : teamInfo[13], 'state' : teamInfo[17], 'arenaName' : teamInfo[18], 'arenaCapacity' : teamInfo[19]}],
+        'activeRoster' : [{'jerseyNumber' : row[9], 'firstName' : row[2], 'lastName' : row[3], 'position' : row[10], 'height' : row[4]} for row in roster]
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
