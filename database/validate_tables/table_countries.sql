cfddd88e682ddb77855cd7fcfc3945b971e3b675
aliemre2023 <aliemrekaya239@gmail.com>
2024-12-29 01:51:02 +0300

    autoincrement id for country table

--- database/validate_tables/table_countries.sql
+++ database/validate_tables/table_countries.sql
@@ -3,15 +3,15 @@ ALTER TABLE countries RENAME TO countries_old;
 
 -- Create the new table with the unique abbreviation constraint
 CREATE TABLE IF NOT EXISTS countries (
-    country_id INT PRIMARY KEY,
+    country_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(100),
     abbreviation VARCHAR(10) UNIQUE, 
     flag_link VARCHAR(255)
 );
 
 -- Insert data from the old table to the new table, handling potential duplicate abbreviations
-INSERT INTO countries (country_id, name, abbreviation, flag_link)
-SELECT country_id, name, abbreviation, flag_link
+INSERT INTO countries (name, abbreviation, flag_link)
+SELECT name, abbreviation, flag_link
 FROM countries_old
 WHERE abbreviation NOT IN (
     SELECT abbreviation FROM countries -- Exclude rows with duplicate abbreviations
