5c4eae445098a0b20d02d2774ddfb505632dc0ba
aliemre2023 <aliemrekaya239@gmail.com>
2024-12-31 23:19:15 +0300

    port switched to 0.0.0.0

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    host = "0.0.0.0"
+    port = int(os.getenv("API_PORT", 5000))  # Default to port 5000
+    app.run(host=host, port=port)
